4b88005e55bc3bbb8898e990504a694bc818d651
youssefea <youcefea99@gmail.com>
2024-08-25 22:14:59 +0100

    add tests

--- src/SuperfluidStaking.sol
+++ src/SuperfluidStaking.sol
@@ -2,11 +2,14 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
 import "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperToken.sol";
 import "@superfluid-finance/ethereum-contracts/contracts/apps/SuperTokenV1Library.sol";
 import "@superfluid-finance/ethereum-contracts/contracts/superfluid/SuperTokenFactory.sol";
+import "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperTokenFactory.sol";
 
 contract ClaimContract {
     ISuperToken public superToken;
@@ -33,33 +36,44 @@ contract ClaimContract {
 contract SuperfluidStaking is Ownable {
     using SuperTokenV1Library for ISuperToken;
 
-    IERC20 public underlyingToken;
+    IERC20 public underlyingStakedToken;
+    IERC20Metadata public underlyingRewardsToken;
     ISuperToken public superToken;
     ISuperfluidPool public pool;
-    SuperTokenFactory public superTokenFactory;
+    ISuperTokenFactory public superTokenFactory;
+    uint128 public scalingFactor;
 
     mapping(address => uint256) public stakedAmounts;
     mapping(address => ClaimContract) public claimContracts;
 
     uint256 public totalStaked;
-    uint256 public rewardRate;
-    uint256 public lastUpdateTime;
-    uint256 public rewardPerTokenStored;
-    mapping(address => uint256) public userRewardPerTokenPaid;
-    mapping(address => uint256) public rewards;
+    int96 public flowRate;
 
     event Staked(address indexed user, uint256 amount);
     event Unstaked(address indexed user, uint256 amount);
     event RewardsClaimed(address indexed user, uint256 amount);
+    event FundsSupplied(uint256 amount, uint256 duration, int96 flowRate);
 
-    constructor(IERC20 _underlyingToken, SuperTokenFactory _superTokenFactory) {
-        underlyingToken = _underlyingToken;
+    constructor(
+        IERC20 _underlyingStakedToken,
+        IERC20Metadata _underlyingRewardsToken,
+        ISuperTokenFactory _superTokenFactory,
+        uint128 _scalingFactor
+    ) {
+        underlyingStakedToken = _underlyingStakedToken;
+        underlyingRewardsToken = _underlyingRewardsToken;
         superTokenFactory = _superTokenFactory;
-        superToken = ISuperToken(superTokenFactory.createCanonicalERC20Wrapper(underlyingToken));
+        scalingFactor = _scalingFactor;
+        superToken = ISuperToken(superTokenFactory.createERC20Wrapper(
+            underlyingRewardsToken,
+            ISuperTokenFactory.Upgradability.SEMI_UPGRADABLE,
+            "Super Rewards",
+            "RWDx"
+        ));
 
         pool = ISuperfluidPool(superToken.createPool(
             address(this),
-            ISuperfluidPool.PoolConfig({
+            PoolConfig({
                 transferabilityForUnitsOwner: false,
                 distributionFromAnyAddress: true
             })
@@ -68,34 +82,32 @@ contract SuperfluidStaking is Ownable {
 
     function supplyFunds(uint256 amount, uint256 duration) external onlyOwner {
         require(duration > 0, "Duration must be greater than 0");
-        updateReward(address(0));
 
-        underlyingToken.transferFrom(msg.sender, address(this), amount);
-        underlyingToken.approve(address(superToken), amount);
+        underlyingRewardsToken.transferFrom(msg.sender, address(this), amount);
+        underlyingRewardsToken.approve(address(superToken), amount);
         superToken.upgrade(amount);
 
-        rewardRate = amount / duration;
-        lastUpdateTime = block.timestamp;
+        int96 newFlowRate = int96(int256(superToken.balanceOf(address(this)) / duration));
+        superToken.distributeFlow(address(this), pool, newFlowRate);
+        
+        flowRate = newFlowRate;
 
-        superToken.distributeFlow(address(this), pool, int96(int256(rewardRate)));
+        emit FundsSupplied(amount, duration, newFlowRate);
     }
 
     function stake(uint256 amount) external {
         require(amount > 0, "Cannot stake 0");
-        updateReward(msg.sender);
 
         totalStaked += amount;
         stakedAmounts[msg.sender] += amount;
 
-        underlyingToken.transferFrom(msg.sender, address(this), amount);
-        underlyingToken.approve(address(superToken), amount);
-        superToken.upgrade(amount);
+        underlyingStakedToken.transferFrom(msg.sender, address(this), amount);
 
         if (address(claimContracts[msg.sender]) == address(0)) {
             claimContracts[msg.sender] = new ClaimContract(superToken, address(this), msg.sender);
         }
 
-        superToken.updateMemberUnits(pool, address(claimContracts[msg.sender]), uint128(stakedAmounts[msg.sender]));
+        superToken.updateMemberUnits(pool, address(claimContracts[msg.sender]), uint128(stakedAmounts[msg.sender])/scalingFactor);
 
         emit Staked(msg.sender, amount);
     }
@@ -103,52 +115,29 @@ contract SuperfluidStaking is Ownable {
     function unstake(uint256 amount) external {
         require(amount > 0, "Cannot unstake 0");
         require(stakedAmounts[msg.sender] >= amount, "Not enough staked");
-        updateReward(msg.sender);
 
         totalStaked -= amount;
         stakedAmounts[msg.sender] -= amount;
 
-        superToken.updateMemberUnits(pool, address(claimContracts[msg.sender]), uint128(stakedAmounts[msg.sender]));
+        superToken.updateMemberUnits(pool, address(claimContracts[msg.sender]), uint128(stakedAmounts[msg.sender])/scalingFactor);
 
         superToken.downgrade(amount);
-        underlyingToken.transfer(msg.sender, amount);
+        underlyingStakedToken.transfer(msg.sender, amount);
 
         emit Unstaked(msg.sender, amount);
     }
 
     function claimRewards() external {
-        updateReward(msg.sender);
-        uint256 reward = rewards[msg.sender];
-        if (reward > 0) {
-            rewards[msg.sender] = 0;
-            ClaimContract claimContract = claimContracts[msg.sender];
-            claimContract.claim();
-            uint256 claimedAmount = superToken.balanceOf(address(claimContract));
-            claimContract.withdrawTo(address(this), claimedAmount);
-            superToken.downgrade(claimedAmount);
-            underlyingToken.transfer(msg.sender, claimedAmount);
-            emit RewardsClaimed(msg.sender, claimedAmount);
-        }
-    }
+        ClaimContract claimContract = claimContracts[msg.sender];
+        require(address(claimContract) != address(0), "No claim contract");
 
-    function updateReward(address account) internal {
-        rewardPerTokenStored = rewardPerToken();
-        lastUpdateTime = block.timestamp;
-        if (account != address(0)) {
-            rewards[account] = earned(account);
-            userRewardPerTokenPaid[account] = rewardPerTokenStored;
-        }
-    }
+        claimContract.claim();
+        uint256 claimedAmount = superToken.balanceOf(address(claimContract));
+        claimContract.withdrawTo(address(this), claimedAmount);
+        superToken.downgrade(claimedAmount);
+        underlyingRewardsToken.transfer(msg.sender, claimedAmount);
 
-    function rewardPerToken() public view returns (uint256) {
-        if (totalStaked == 0) {
-            return rewardPerTokenStored;
-        }
-        return rewardPerTokenStored + (((block.timestamp - lastUpdateTime) * rewardRate * 1e18) / totalStaked);
-    }
-
-    function earned(address account) public view returns (uint256) {
-        return ((stakedAmounts[account] * (rewardPerToken() - userRewardPerTokenPaid[account])) / 1e18) + rewards[account];
+        emit RewardsClaimed(msg.sender, claimedAmount);
     }
 
     function emergencyWithdraw(IERC20 token) external onlyOwner {
@@ -165,8 +154,8 @@ contract SuperfluidStaking is Ownable {
         return totalStaked;
     }
 
-    function getRewardRate() external view returns (uint256) {
-        return rewardRate;
+    function getFlowRate() external view returns (int96) {
+        return flowRate;
     }
 
     function getClaimContract(address user) external view returns (address) {
---
+++ test/SuperfluidStaking.t.sol
@@ -0,0 +1,229 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/SuperfluidStaking.sol";
+import { ERC1820RegistryCompiled } from "@superfluid-finance/ethereum-contracts/contracts/libs/ERC1820RegistryCompiled.sol";
+import "@superfluid-finance/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
+import "@superfluid-finance/ethereum-contracts/contracts/utils/SuperfluidFrameworkDeployer.sol";
+import "@superfluid-finance/ethereum-contracts/contracts/utils/SuperfluidFrameworkDeploymentSteps.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+contract MockERC20 is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        _mint(msg.sender, 1000000 * 10**decimals());
+    }
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
+
+contract SuperfluidStakingTest is Test {
+    SuperfluidStaking public sfStaking;
+    SuperfluidFrameworkDeployer.Framework public sf;
+    MockERC20 public stakedToken;
+    MockERC20 public rewardsToken;
+    ISuperToken public superRewardsToken;
+
+    address public owner;
+    address public alice;
+    address public bob;
+
+    uint128 public constant SCALING_FACTOR = 1e10;
+    uint256 public constant INITIAL_BALANCE = 1000000 * 1e18;
+    uint256 public constant STAKE_AMOUNT = 1000 * 1e18;
+
+    function setUp() public {
+        vm.etch(ERC1820RegistryCompiled.at, ERC1820RegistryCompiled.bin);
+        SuperfluidFrameworkDeployer sfDeployer = new SuperfluidFrameworkDeployer();
+        sfDeployer.deployTestFramework();
+        sf = sfDeployer.getFramework();
+
+        owner = address(this);
+        alice = address(0x1);
+        bob = address(0x2);
+
+        stakedToken = new MockERC20("Staked Token", "STK");
+        
+        rewardsToken = new MockERC20("Rewards Token", "RWD");
+
+        sfStaking = new SuperfluidStaking(
+            IERC20(address(stakedToken)),
+            IERC20Metadata(address(rewardsToken)),
+            sf.superTokenFactory,
+            SCALING_FACTOR
+        );
+
+        superRewardsToken = sfStaking.superToken();
+
+        // Mint tokens to users
+        stakedToken.mint(alice, INITIAL_BALANCE);
+        stakedToken.mint(bob, INITIAL_BALANCE);
+        rewardsToken.mint(owner, INITIAL_BALANCE);
+
+        // Approve staking contract
+        vm.startPrank(alice);
+        stakedToken.approve(address(sfStaking), type(uint256).max);
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        stakedToken.approve(address(sfStaking), type(uint256).max);
+        vm.stopPrank();
+
+        vm.startPrank(owner);
+        rewardsToken.approve(address(sfStaking), type(uint256).max);
+        vm.stopPrank();
+    }
+
+    function testSupplyFunds() public {
+        uint256 amount = 10000 * 1e18;
+        uint256 duration = 30 days;
+
+        vm.startPrank(owner);
+        sfStaking.supplyFunds(amount, duration);
+        vm.stopPrank();
+
+        assertEq(sfStaking.getFlowRate(), int96(int256(amount / duration)));
+        assertEq(superRewardsToken.balanceOf(address(sfStaking.pool())), amount);
+    }
+
+    function testStake() public {
+        vm.startPrank(alice);
+        sfStaking.stake(STAKE_AMOUNT);
+        vm.stopPrank();
+
+        assertEq(sfStaking.getStakedAmount(alice), STAKE_AMOUNT);
+        assertEq(sfStaking.getTotalStaked(), STAKE_AMOUNT);
+        assertEq(stakedToken.balanceOf(address(sfStaking)), STAKE_AMOUNT);
+    }
+
+    function testUnstake() public {
+        vm.startPrank(alice);
+        sfStaking.stake(STAKE_AMOUNT);
+        sfStaking.unstake(STAKE_AMOUNT / 2);
+        vm.stopPrank();
+
+        assertEq(sfStaking.getStakedAmount(alice), STAKE_AMOUNT / 2);
+        assertEq(sfStaking.getTotalStaked(), STAKE_AMOUNT / 2);
+        assertEq(stakedToken.balanceOf(address(sfStaking)), STAKE_AMOUNT / 2);
+    }
+
+    function testClaimRewards() public {
+        uint256 supplyAmount = 10000 * 1e18;
+        uint256 duration = 30 days;
+
+        // Supply funds
+        vm.startPrank(owner);
+        sfStaking.supplyFunds(supplyAmount, duration);
+        vm.stopPrank();
+
+        // Stake tokens
+        vm.startPrank(alice);
+        sfStaking.stake(STAKE_AMOUNT);
+        vm.stopPrank();
+
+        // Advance time
+        vm.warp(block.timestamp + 7 days);
+
+        // Claim rewards
+        vm.startPrank(alice);
+        sfStaking.claimRewards();
+        vm.stopPrank();
+
+        // Check rewards (this is an approximation)
+        uint256 expectedRewards = (supplyAmount * 7 days) / duration;
+        uint256 actualRewards = rewardsToken.balanceOf(alice);
+        assertApproxEqRel(actualRewards, expectedRewards, 1e16); // 1% tolerance
+    }
+
+    function testMultipleStakers() public {
+        uint256 supplyAmount = 10000 * 1e18;
+        uint256 duration = 30 days;
+
+        // Supply funds
+        vm.startPrank(owner);
+        sfStaking.supplyFunds(supplyAmount, duration);
+        vm.stopPrank();
+
+        // Alice stakes
+        vm.startPrank(alice);
+        sfStaking.stake(STAKE_AMOUNT);
+        vm.stopPrank();
+
+        // Bob stakes
+        vm.startPrank(bob);
+        sfStaking.stake(STAKE_AMOUNT * 2);
+        vm.stopPrank();
+
+        assertEq(sfStaking.getTotalStaked(), STAKE_AMOUNT * 3);
+
+        // Advance time
+        vm.warp(block.timestamp + 15 days);
+
+        // Both users claim rewards
+        vm.startPrank(alice);
+        sfStaking.claimRewards();
+        vm.stopPrank();
+
+        vm.startPrank(bob);
+        sfStaking.claimRewards();
+        vm.stopPrank();
+
+        // Check rewards distribution (this is an approximation)
+        uint256 totalRewards = (supplyAmount * 15 days) / duration;
+        uint256 aliceRewards = rewardsToken.balanceOf(alice);
+        uint256 bobRewards = rewardsToken.balanceOf(bob);
+
+        assertApproxEqRel(aliceRewards, totalRewards / 3, 1e16); // 1% tolerance
+        assertApproxEqRel(bobRewards, (totalRewards * 2) / 3, 1e16); // 1% tolerance
+    }
+
+    function testEmergencyWithdraw() public {
+        uint256 amount = 1000 * 1e18;
+        rewardsToken.transfer(address(sfStaking), amount);
+
+        vm.startPrank(owner);
+        sfStaking.emergencyWithdraw(IERC20(address(rewardsToken)));
+        vm.stopPrank();
+
+        assertEq(rewardsToken.balanceOf(owner), INITIAL_BALANCE);
+    }
+
+    function testGetters() public {
+        vm.startPrank(alice);
+        sfStaking.stake(STAKE_AMOUNT);
+        vm.stopPrank();
+
+        assertEq(sfStaking.getStakedAmount(alice), STAKE_AMOUNT);
+        assertEq(sfStaking.getTotalStaked(), STAKE_AMOUNT);
+        assertEq(sfStaking.getFlowRate(), 0); // No funds supplied yet
+        assertTrue(sfStaking.getClaimContract(alice) != address(0));
+    }
+
+    function testFailStakeZero() public {
+        vm.startPrank(alice);
+        sfStaking.stake(0);
+        vm.stopPrank();
+    }
+
+    function testFailUnstakeMoreThanStaked() public {
+        vm.startPrank(alice);
+        sfStaking.stake(STAKE_AMOUNT);
+        sfStaking.unstake(STAKE_AMOUNT + 1);
+        vm.stopPrank();
+    }
+
+    function testFailClaimWithoutStaking() public {
+        vm.startPrank(alice);
+        sfStaking.claimRewards();
+        vm.stopPrank();
+    }
+
+    function testFailEmergencyWithdrawNonOwner() public {
+        vm.startPrank(alice);
+        sfStaking.emergencyWithdraw(IERC20(address(rewardsToken)));
+        vm.stopPrank();
+    }
+}
